1e80cde2c59484003b7e839698eed10837b004d2
noother <noothy@gmail.com>
2011-06-26 16:44:51 +0200

    split long PRIVMSGs in several PRIVMSGs with optimal length

--- classes/IRC_Target.php
+++ classes/IRC_Target.php
@@ -12,7 +12,29 @@ abstract class IRC_Target {
 	public $data = array(); // This unused property is meant for plugins to write to
 	
 	public function privmsg($message) {
-		$this->Server->sendRaw('PRIVMSG '.$this->name.' :'.$message);
+		/*
+			irc max message length is 512 bytes including CRLF
+			The irc message received by clients counts - NOT what we send
+			Example: `:Nimda3!~Nimda3@unaffiliated/noother/bot/nimda PRIVMSG #nimda :Pong!`
+			So, max privmsg-length (only text) =
+				512
+				- 1 (the starting :)
+				- length(bots_banmask)
+				- 9 (' PRIVMSG ')
+				- length(target)
+				- 2 (' :')
+				- 2 (CRLF)
+		*/
+		
+		$max_length = 498 - strlen($this->Server->myBanmask) - strlen($this->name); 
+		
+		do {
+			$to_send = substr($message, 0, $max_length);
+			$this->Server->sendRaw('PRIVMSG '.$this->name.' :'.$to_send);
+			
+			$message = substr($message, $max_length);
+		} while(!empty($message));
+		
 	}
 	
 	public function sendCTCP($message) {
