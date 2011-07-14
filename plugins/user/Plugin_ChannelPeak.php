b0b83d25a1bd460c9e6d34bfd0f5993b3fed6c67
noother <noothy@gmail.com>
2011-07-14 08:24:19 +0200

    converted ChannelPeak-Plugin to the new memory-system

--- plugins/user/Plugin_ChannelPeak.php
+++ plugins/user/Plugin_ChannelPeak.php
@@ -10,53 +10,38 @@ class Plugin_ChannelPeak extends Plugin {
 			return;
 		}
 		
-		$peak = $this->Channel->data['peak'];
-		
 		$this->reply(sprintf(
 			"Current channel peak for \x02%s\x02: %d users online at %s (%s ago)",
 				$this->Channel->name,
-				$peak['peak'],
-				$peak['date'],
-				libTime::secondsToString(libTime::getSecondsDifference(date('r'), $peak['date']))
+				$this->Channel->getVar('peak'),
+				$this->Channel->getVar('peak_date'),
+				libTime::secondsToString(libTime::getSecondsDifference(date('r'), $this->Channel->getVar('peak_date')))
 		));
 	}
 	
 	function onJoin() {
 		$user_count = sizeof($this->Channel->users);
 		
-		if($user_count > $this->Channel->data['peak']['peak']) {
-			$old_peak = $this->Channel->data['peak'];
-			
-			$this->MySQL->query("UPDATE `channelpeaks` SET `peak` = ".$user_count.", `date` = NOW() WHERE `server` = '".addslashes($this->Server->host)."' AND `channel` = '".addslashes($this->Channel->name)."'");
-			$this->Channel->data['peak'] = array('peak' => $user_count, 'date' => date('Y-m-d H:i:s'));
-			
+		if($user_count > $this->Channel->getVar('peak')) {
 			$this->reply(sprintf(
 				"New channel peak for \x02%s\x02: %d users online. Old one was %d users online at %s (%s ago)",
 					$this->Channel->name,
 					$user_count,
-					$old_peak['peak'],
-					$old_peak['date'],
-					libTime::secondsToString(libTime::getSecondsDifference(date('r'), $old_peak['date']))
+					$this->Channel->getVar('peak'),
+					$this->Channel->getVar('peak_date'),
+					libTime::secondsToString(libTime::getSecondsDifference(date('r'), $this->Channel->getVar('peak_date')))
 			));
+			
+			$this->Channel->saveVar('peak', $user_count);
+			$this->Channel->saveVar('peak_date', date('Y-m-d H:i:s'));
 		}
 	}
 	
 	function onMeJoin() {
-		$peak = $this->MySQL->fetchRow("SELECT `peak`, `date` FROM `channelpeaks` WHERE `server` = '".addslashes($this->Server->host)."' AND `channel` = '".addslashes($this->Channel->name)."'");
-		
-		if(!$peak) {
+		if(!$this->Channel->getVar('peak')) {
 			$user_count = sizeof($this->Channel->users);
-			
-			$this->MySQL->query("INSERT INTO `channelpeaks` (`server`, `channel`, `peak`, `date`) VALUES (
-				'".addslashes($this->Server->host)."',
-				'".addslashes($this->Channel->name)."',
-				".$user_count.",
-				NOW()
-			)");
-			
-			$this->Channel->data['peak'] = array('peak' => $user_count, 'date' => date('Y-m-d H:i:s'));
-		} else {
-			$this->Channel->data['peak'] = $peak;
+			$this->Channel->saveVar('peak', $user_count);
+			$this->Channel->saveVar('peak_date', date('Y-m-d H:i:s'));
 		}
 	}
 	
