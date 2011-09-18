9cfa45d793a4d98e57d79441dc49f4887a8154a6
noother <noothy@gmail.com>
2011-09-18 10:55:08 +0200

    ops

--- libs/libString.php
+++ libs/libString.php
@@ -65,10 +65,10 @@ class libString {
 		
 		if($word == 'time') {
 			switch($num) {
-				case 1: return 'once';
-				case 2: return 'twice';
-				case 3: return 'thrice';
-				return $word.'s';
+				case 1:  return 'once';
+				case 2:  return 'twice';
+				case 3:  return 'thrice';
+				default: return $word.'s';
 			}
 		}
 		
