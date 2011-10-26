2702f174122292ed0e1a27c54fbccb755877b146
noother <noothy@gmail.com>
2011-10-26 23:00:03 +0200

    get_microtime doesnt exist

--- libs/libCrypt.php
+++ libs/libCrypt.php
@@ -3,11 +3,11 @@
 class libCrypt {
 	
 	static function createSalt() {
-		return substr(crypt(md5(get_microtime()),CRYPT_STD_DES),2,8);
+		return substr(crypt(md5(rand()),CRYPT_STD_DES),2,8);
 	}
 	
 	static function getRandomHash() { 
-		return md5(get_microtime());
+		return md5(rand());
 	}
 	
 	static function createPassword() {
