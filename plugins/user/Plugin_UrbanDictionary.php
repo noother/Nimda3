90aa9732fdc4e0f4f39cbe288fcc52d1a310b1fa
noother <noothy@gmail.com>
2011-10-27 13:00:20 +0200

    made UrbanDictionary use jobs

--- plugins/user/Plugin_UrbanDictionary.php
+++ plugins/user/Plugin_UrbanDictionary.php
@@ -12,16 +12,40 @@ class Plugin_UrbanDictionary extends Plugin {
 		}
 		
 		$term = $this->data['text'];
+		$this->addJob('getDefinition', $term);
 		
-		$html = libHTTP::GET('http://www.urbandictionary.com/define.php?term='.urlencode($term));
-		if($html === false) {
+	}
+	
+	function onJobDone() {
+		$data = &$this->data['result'];
+		if(isset($data['timeout'])) {
 			$this->reply('Timeout on contacting urbandictionary.com');
 			return;
 		}
 		
-		if(strstr($html, "isn't defined <a href")) {
-			$this->reply($term.' has no definition. Feel free to add one at http://www.urbandictionary.com/add.php?word='.$term);
+		if(isset($data['undefined'])) {
+			$this->reply($data['term'].' has no definition. Feel free to add one at http://www.urbandictionary.com/add.php?word='.urlencode($data['term']));
 			return;
+		}
+		
+		$this->reply($data['definition']);
+		if(isset($data['example'])) {
+			$this->reply("\x02Example:\x02 ".$data['example']);
+		}
+	}
+	
+	public function getDefinition($term) {
+		$data = array('term' => $term);
+		
+		$html = libHTTP::GET('http://www.urbandictionary.com/define.php?term='.urlencode($term));
+		if($html === false) {
+			$data['timeout'] = true;
+			return $data;
+		}
+		
+		if(strstr($html, "isn't defined <a href")) {
+			$data['undefined'] = true;
+			return $data;
 		}
 		
 		preg_match('#<div class="definition">(.+?)</div>.*?<div class="example">(.*?)</div>#s', $html, $arr);
@@ -30,7 +54,7 @@ class Plugin_UrbanDictionary extends Plugin {
 		$definition = strtr($definition, array("\r" => ' ', "\n" => ' '));
 		while(false !== strstr($definition, '  ')) $definition = str_replace('  ', ' ', $definition);
 		if(strlen($definition) > 800) $definition = substr($definition, 0 ,800).'...';
-		$this->reply($definition);
+		$data['definition'] = $definition;
 		
 		if(!empty($arr[2])) {
 			$example = trim(html_entity_decode(strip_tags(br2nl($arr[2]))));
@@ -38,8 +62,10 @@ class Plugin_UrbanDictionary extends Plugin {
 			while(false !== strstr($example, ' |  | ')) $example = str_replace(' |  | ', ' | ', $example);
 			while(false !== strstr($example, '  '))     $example = str_replace('  ', ' ', $example);
 			if(strlen($example) > 800) $example = substr($example, 0, 800).'...';
-			$this->reply("\x02Example:\x02 ".$example);
+			$data['example'] = $example;
 		}
+		
+	return $data;
 	}
 }
 
