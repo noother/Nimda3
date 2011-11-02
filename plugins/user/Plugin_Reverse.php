65015ecf963c7cbeaa6d0620779a81cc38a6d4a1
noother <noothy@gmail.com>
2011-11-02 23:09:58 +0100

    added usage to reverse

--- plugins/user/Plugin_Reverse.php
+++ plugins/user/Plugin_Reverse.php
@@ -5,11 +5,12 @@ class Plugin_Reverse extends Plugin {
 	public $triggers = array('!reverse');
 	
 	public $helpCategory = 'Cryptography';
+	public $usage = '<string>';
 	public $helpText = 'Uses one of the most efficient encodings - It sends back your string reversed.';
 	
 	function isTriggered() {
 		if(!isset($this->data['text'])) {
-			$this->reply('Usage: !reverse <text>');
+			$this->printUsage();
 			return;
 		}
 
