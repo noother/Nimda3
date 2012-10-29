57380be56398543856663a12de26e58b355d54d0
noother <noothy@gmail.com>
2012-10-30 00:03:27 +0100

    ZMachine: Removed captures for quit and restart, since press-a-key-messages are now supported

--- plugins/user/Plugin_ZMachine.php
+++ plugins/user/Plugin_ZMachine.php
@@ -1,4 +1,4 @@
-<?php
+a<?php
 
 require_once('ZMachine/ZMachine.php');
 
@@ -151,7 +151,7 @@ class Plugin_ZMachine extends Plugin {
 			return false;
 		}
 		
-		$this->reply("Starting \x02".$info['name']."\x02.".($this->Channel ? ' Use !z <command> to interact.' : ''). " Typical commands are look, look at the mailbox, open mailbox, get leaflet, read leaflet, go south, etc. | Special commands are \x02save\x02, \x02load\x02, \x02restart\x02, \x02quit\x02 & \x02RETURN\02");
+		$this->reply("Starting \x02".$info['name']."\x02.".($this->Channel ? ' Use !z <command> to interact.' : ''). " Typical commands are look, look at the mailbox, open mailbox, get leaflet, read leaflet, go south, etc. | Special commands are \x02save\x02, \x02load\x02, & \x02RETURN\02");
 		
 		if(empty($this->sessions)) $this->interval = 1;
 		$this->sessions[$session_id] = array(
@@ -231,7 +231,6 @@ class Plugin_ZMachine extends Plugin {
 				break;
 				
 				case 'close':
-					$Target->privmsg('The process got shutdown unexpectedly.');
 					$this->removeSession($session_id);
 				break 2;
 			}
@@ -319,12 +318,6 @@ class Plugin_ZMachine extends Plugin {
 				}
 			break;
 			
-			case 'restart': case 'reset':
-				$game = $this->sessions[$session_id]['game_name'];
-				$this->removeSession($session_id);
-				$this->startSession($session_id, $game);
-			break;
-			
 			case 'RETURN': case 'ENTER':
 				$G->write('');
 			break;
@@ -333,9 +326,6 @@ class Plugin_ZMachine extends Plugin {
 				$T->privmsg('Unsupported.');
 			break;
 			
-			case 'quit': case 'q': case 'die':
-				$this->removeSession($session_id);
-			return;
 			default:
 				$G->write($command);
 			break;
