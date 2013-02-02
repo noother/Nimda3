f334e8013d3d332c26028d9b577c069d375b1634
noother <noothy@gmail.com>
2013-02-03 00:40:53 +0100

    Plugin_UrbanDictionary: Added html escaped hexvalue decoding

--- plugins/user/Plugin_UrbanDictionary.php
+++ plugins/user/Plugin_UrbanDictionary.php
@@ -56,6 +56,7 @@ class Plugin_UrbanDictionary extends Plugin {
 		
 		$definition = trim(html_entity_decode(strip_tags(br2nl($arr[1]))));
 		$definition = strtr($definition, array("\r" => ' ', "\n" => ' '));
+		$definition = preg_replace_callback('/&#([a-z0-9]+);/i', create_function('$a', 'return chr(hexdec($a[0]));'), $definition);
 		while(false !== strstr($definition, '  ')) $definition = str_replace('  ', ' ', $definition);
 		if(strlen($definition) > 800) $definition = substr($definition, 0 ,800).'...';
 		$data['definition'] = $definition;
@@ -63,6 +64,7 @@ class Plugin_UrbanDictionary extends Plugin {
 		if(!empty($arr[2])) {
 			$example = trim(html_entity_decode(strip_tags(br2nl($arr[2]))));
 			$example = strtr($example, array("\r" => ' | ', "\n" => ' | '));
+			$example = preg_replace_callback('/&#([a-z0-9]+);/i', create_function('$a', 'return chr(hexdec($a[0]));'), $example);
 			while(false !== strstr($example, ' |  | ')) $example = str_replace(' |  | ', ' | ', $example);
 			while(false !== strstr($example, '  '))     $example = str_replace('  ', ' ', $example);
 			if(strlen($example) > 800) $example = substr($example, 0, 800).'...';
