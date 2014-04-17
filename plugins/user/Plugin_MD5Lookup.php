9ba7507a17f2a441bed5cc4197496129b568773e
noother <noothy@gmail.com>
2014-04-17 10:19:47 +0200

    cleaned up MD5 lookup (removed not working services and added a few new)

--- plugins/user/Plugin_MD5Lookup.php
+++ plugins/user/Plugin_MD5Lookup.php
@@ -38,26 +38,16 @@ class Plugin_MD5Lookup extends Plugin {
 		$result = array('hash' => $hash);
 		
 		$url = false;
-		if(false !== $plain = $this->checkRednoize($hash)) {
-			$url = 'http://md5.rednoize.com';
-		} elseif(false !== $plain = $this->checkMD5MyAddr($hash)) {
+		if(false !== $plain = $this->checkMD5MyAddr($hash)) {
 			$url = 'http://md5.my-addr.com';
-		} elseif(false !== $plain = $this->checkPasscracking($hash)) {
-			$url = 'http://passcracking.com';
-		} elseif(false !== $plain = $this->checkHashcrack($hash)) {
-			$url = 'http://hashcrack.com';
 		} elseif(false !== $plain = $this->checkMD5Decryption($hash)) {
 			$url = 'http://md5decryption.com';
-		} elseif(false !== $plain = $this->checkMD5net($hash)) {
-			$url = 'http://www.md5.net';
-		} elseif(false !== $plain = $this->checkMD5Online($hash)) {
+		} elseif(false !== $plain = $this->checkMD5OnlineNet($hash)) {
 			$url = 'http://md5online.net';
-		} elseif(false !== $plain = $this->checkMD5Hood($hash)) {
-			$url = 'http://md5hood.com';
-		} elseif(false !== $plain = $this->checkTMTO($hash)) {
-			$url = 'http://www.tmto.org';
-		} elseif(false !== $plain = $this->checkEnigmagroup($hash)) {
-			$url = 'http://www.enigmagroup.org';
+		} elseif(false !== $plain = $this->checkMD5Decrypt($hash)) {
+			$url = 'http://www.md5decrypt.org';
+		} elseif(false !== $plain = $this->checkMD5OnlineOrg($hash)) {
+			$url = 'http://md5online.org';
 		}
 		
 		$result['plain'] = $plain;
@@ -66,44 +56,11 @@ class Plugin_MD5Lookup extends Plugin {
 	return $result;
 	}
 	
-	private function checkRednoize($hash) {
-		$html = libHTTP::GET('http://md5.rednoize.com/?p&s=md5&q='.$hash.'&_=');
-		if($html === false) return false;
-		
-		if(!empty($html)) {
-			return $html;
-		} else {
-			return false;
-		}
-	}
-	
 	private function checkMD5MyAddr($hash) {
 		$html = libHTTP::POST('http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php', array('md5' => $hash));
 		if($html === false) return false;
 		
 		if(preg_match('#Hashed string</span>: (.*?)</div>#', $html, $arr)) {
-			return $arr[1];
-		} else {
-			return false;
-		}
-	}
-	
-	private function checkPasscracking($hash) {
-		$html = libHTTP::POST('http://passcracking.com/index.php', array('datafromuser' => $hash));
-		if($html === false) return false;
-		
-		if(preg_match('#<td bgcolor=\#FF0000>(.*?)</td>#', $html, $arr)) {
-			return $arr[1];
-		} else {
-			return false;
-		}
-	}
-	
-	private function checkHashcrack($hash) {
-		$html = libHTTP::POST('http://hashcrack.com/indx.php', array('hash' => $hash, 'auth' => 'fail'));
-		if($html === false) return false;
-		
-		if(preg_match('#<span class=hervorheb2>(.*?)</span>#', $html, $arr)) {
 			return $arr[1];
 		} else {
 			return false;
@@ -121,20 +78,7 @@ class Plugin_MD5Lookup extends Plugin {
 		}
 	}
 	
-	private function checkMD5net($hash) {
-		$html = libHTTP::POST('http://www.md5.net/cracker.php', array('hash' => $hash));
-		if($html === false) return false;
-		
-		if(!preg_match('#<input type="text" id="hash" size="32" value="(.*?)"/>#', $html, $arr)) return false;
-		
-		if($arr[1] != 'Entry not found.') {
-			return $arr[1];
-		} else {
-			return false;
-		}
-	}
-	
-	private function checkMD5Online($hash) {
+	private function checkMD5OnlineNet($hash) {
 		$html = libHTTP::POST('http://md5online.net/', array('pass' => $hash, 'option' => 'hash2text'));
 		if($html === false) return false;
 		
@@ -145,47 +89,33 @@ class Plugin_MD5Lookup extends Plugin {
 		}
 	}
 	
-	private function checkMD5Hood($hash) {
-		$html = libHTTP::POST('http://md5hood.com/index.php/cracker/crack', array('md5' => $hash, 'submit' => 'Go'));
+	private function checkMD5OnlineOrg($hash) {
+		$html = libHTTP::POST('http://md5online.org/', array('md5' => $hash, 'action' => 'decrypt', 'a' => '123456')); // csrf token is not validated
 		if($html === false) return false;
 		
-		if(preg_match('#<div class="result_true">(.*?)</div>#', $html, $arr)) {
+		if(preg_match('#Found : <b>(.+?)</b>#', $html, $arr)) {
 			return $arr[1];
 		} else {
 			return false;
 		}
 	}
 	
-	private function checkTMTO($hash) {
-		$html = libHTTP::GET('http://www.tmto.org/api/latest/?hash='.$hash.'&auth=true');
+	private function checkMD5Decrypt($hash) {
+		$HTTP = new HTTP('www.md5decrypt.org');
+		$html = $HTTP->GET('/');
 		if($html === false) return false;
+		if(!preg_match('/<script>document.cookie=\'(.+?)=(.+?)\';/', $html, $arr)) return false;
 		
-		if(!preg_match('#text="(.*?)"#', $html, $arr)) return false;
+		$HTTP->setCookie($arr[1], $arr[2]);
 		
-		if(!empty($arr[1])) {
-			return base64_decode($arr[1]);
-		} else {
-			return false;
-		}
-	}
-	
-	private function checkEnigmagroup($hash) {
-		$HTTP = new HTTP('www.enigmagroup.org');
-		$html = $HTTP->GET('/pages/cracker/');
-		if($html === false) return false;
+		$res = $HTTP->POST('/index/process', array('value' => base64_encode($hash), 'operation' => 'MD5D'));
+		if($res === false) return false;
 		
-		preg_match('#<input type="hidden" name="tk" value="(.+?)"#', $html, $arr);
-		$token = $arr[1];
+		$json = json_decode($res);
+		if(!$json) return false;
+		if(!empty($json->error)) return false;
 		
-		$html = $HTTP->POST('/pages/cracker/', array('tk' => $token, 'shash' => $hash, 'type' => 'md5', 'submit_shash' => 'Crack Me'));
-		if($html === false) return false;
-		
-		if(preg_match('#<font color="\#66aa00">MD5: .+? = (.*?)</font>#', $html, $arr)) {
-			if($arr[1] == 'Hash not found!') return false;
-			else return $arr[1];
-		}
-		
-	return false;
+	return $json->body;
 	}
 	
 }
